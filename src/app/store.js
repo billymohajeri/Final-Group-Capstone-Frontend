f47d204ef642e49b9b61baed3846884d18c4759e
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-05-15 15:17:54 +0300

    Update styling

---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { authLog } from '../api/authLog';
+
+const store = configureStore({
+  reducer: {
+    [authLog.reducerPath]: authLog.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(authLog.middleware),
+});
+setupListeners(store.dispatch);
+
+export default store;
