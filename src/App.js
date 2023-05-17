3392b891199764057966963fab0514ae4468aae0
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-05-17 14:24:17 +0530

    Add routes for reservation page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './components/Home';
 import Login from './components/Login';
 import './App.css';
 import AddRoom from './pages/AddRoom';
 import DeleteRoom from './components/DeleteRoom';
+import RoomDetails from './components/Detail';
+import Reservation from './pages/Reservation';
 
 function App() {
+  const [roomId, setRoomId] = useState('');
+  const handleClick = (data) => {
+    setRoomId(data);
+  };
+
   return (
     <Router>
       <Routes>
@@ -14,6 +22,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/room/new" element={<AddRoom />} />
         <Route path="/delete" element={<DeleteRoom />} />
+        <Route path="reservation/new" element={<Reservation roomId={roomId} />} />
+        <Route exact path="/room/:id" element={<RoomDetails onButtonReservedClick={handleClick} />} />
       </Routes>
     </Router>
   );
