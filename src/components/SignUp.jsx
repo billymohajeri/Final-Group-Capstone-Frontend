20c27d31fb33074709827bdff9b90839800e444d
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-05-23 11:42:58 +0300

    Update signup page styling

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
-  FaLock,
-  FaUser,
+  // FaLock,
+  FaGripLines,
+  FaSearch,
   FaUserPlus,
   FaChevronCircleRight,
 } from 'react-icons/fa';
-import { MdHotel } from 'react-icons/md';
+// import { MdHotel } from 'react-icons/md';
 import './css/signup.css';
 
 function SignUp() {
@@ -39,53 +40,65 @@ function SignUp() {
   };
 
   return (
-    <div className="containerForm">
-      <div className="formCard">
-        <form className="signupForm" onSubmit={handleSubmit}>
-          <MdHotel className="bed" />
-          <h2>Hotel Booking</h2>
-          <div className="login__field">
-            <FaUser />
-            <input
-              type="text"
-              className="login__input"
-              id="username"
-              name="username"
-              value={username}
-              placeholder="Username"
-              onChange={(event) => setUsername(event.target.value)}
-            />
-          </div>
-          <div className="login__field">
-            <FaLock />
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              className="login__input"
-              placeholder="Password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          <div className="login__field">
-            <FaLock />
-            <input
-              type="password"
-              className="login__input"
-              value={passwordConfirmation}
-              placeholder="Confirm password"
-              onChange={(event) => setPasswordConfirmation(event.target.value)}
-            />
-          </div>
-          <button className="myButton buttonForm" type="submit">
-            <FaUserPlus />
-            Sign Up
-            <FaChevronCircleRight />
-          </button>
-          {error && <div>{error}</div>}
-        </form>
-        <img alt="side background" src="./cover.jpg" />
+    <div
+      className="login-background"
+      style={{
+        backgroundImage: 'url(/login.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="menu-buttons">
+        <FaGripLines />
+      </div>
+      <form className="signupForm" onSubmit={handleSubmit}>
+        {/* <MdHotel className="bed" /> */}
+        <h2>Create a new account</h2>
+        <div className="login__field">
+          {/* <FaUser /> */}
+          <input
+            type="text"
+            className="login__input"
+            id="username"
+            name="username"
+            value={username}
+            placeholder="Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="login__field">
+          {/* <FaLock /> */}
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            className="login__input"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="login__field">
+          {/* <FaLock /> */}
+          <input
+            type="password"
+            className="login__input"
+            value={passwordConfirmation}
+            placeholder="Confirm password"
+            onChange={(event) => setPasswordConfirmation(event.target.value)}
+          />
+        </div>
+        <button className="myButton" type="submit">
+          <FaUserPlus />
+          Sign Up
+          <FaChevronCircleRight />
+        </button>
+        {error && <div>{error}</div>}
+      </form>
+      <div className="menu-buttons2">
+        <div className="glass">
+          <FaSearch />
+        </div>
       </div>
     </div>
   );
